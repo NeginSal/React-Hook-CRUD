bc3b72f67c6cef01cd62fabccd14e45c5c25cff8
NeginSal <neginsaljooghi@gmail.com>
2023-05-27 17:30:01 +0330

    Edited UserList

--- src/components/UserList.js
+++ src/components/UserList.js
@@ -1,47 +1,48 @@
+import axios from "axios";
 import { useEffect, useState } from "react";
 
 const UserList = () => {
-  // return (
-  //   <div className='py-5'>
-  //     {users.length > 0 ?
-  //       <div>
-  //         {
-  //           users.map((user) => {
-  //             return (
-  //               <ul key={user.id}>
-  //                 <li className='flex justify-evenly border-b-2'>
-  //                   <p className='mx-5 my-3 px-5'>{user.name}</p>
-  //                   <p className='mx-5 my-3 px-5'>{user.username}</p>
-  //                   <button
-  //                     className='mx-5 my-3 px-5 py-1 text-green-800
-  //                     hover:bg-green-400 hover:rounded-md hover:border hover:border-green-800'
-  //                     onClick={() => editRow(user)}
-  //                   >EDIT</button>
-  //                   <button
-  //                     className='mx-5 my-3 px-5 py-1 text-red-800
-  //                      hover:bg-red-400 hover:rounded-md hover:border hover:border-red-800'
-  //                     onClick={() => deleteUser(user.id)}
-  //                   >Delete</button>
-  //                 </li>
-  //               </ul>
-  //             )
-  //           })
-  //         }
-  //       </div>
-  //       :
-  //       <div className='text-center text-2xl font-semi-bold my-5'>
-  //         <p>Ooops! There is no user !</p>
-  //       </div>
-  //     }
-  //   </div>
-  // )
   const [users, setUsers] = useState([]);
-  useEffect(()=>{},[])
-  return(
-    <div>
-      
+  const [loading, setLoading] = useState(true);
+
+  useEffect(() => {
+    setLoading(true)
+    axios.get("http://localhost:8000/users")
+      .then(response => {
+        setUsers(response.data)
+      })
+    setLoading(false)
+  }, [])
+
+  return (
+    <div className='py-5'>
+      {loading && <p>loading ...</p>}
+      {!loading &&
+        <div>
+          {(users.map((user) =>
+            <div key={user.id} >
+              <ul>
+                <li className='flex justify-evenly border-b-2'>
+                  <p className='mx-5 my-3 px-5'>{user.name}</p>
+                  <p className='mx-5 my-3 px-5'>{user.username}</p>
+                  <button
+                    className='mx-5 my-3 px-5 py-1 text-green-800
+                       hover:bg-green-400 hover:rounded-md hover:border hover:border-green-800'
+                  // onClick={() => editRow(user)}
+                  >EDIT</button>
+                  <button
+                    className='mx-5 my-3 px-5 py-1 text-red-800
+                        hover:bg-red-400 hover:rounded-md hover:border hover:border-red-800'
+                  // onClick={() => deleteUser(user.id)}
+                  >Delete</button>
+                </li>
+              </ul>
+            </div >
+          ))}
+        </div >
+      }
     </div>
   )
 }
- 
+
 export default UserList;
